e177ac7fb98d2021c614d7f8acf883b6d74d2a6a
najah <najahaboobacker79@gmail.com>
2023-09-10 16:53:45 +0530

    123

--- hello.py
+++ hello.py
@@ -1,8 +1,23 @@
 #ask user for thier name
 name=input("what is your name")
-
+#remove white space from string
+name=name.strip()
+#capitalize the string
+name=name.capitalize()
+#capitalize the first letter of the word
+name=name.title()
+#we can also remove white space as weell as capitalize
+name1=name.strip().title()
+#in single line both stripping whitespcae and capitalizing
+string=input("any string").strip().title()
+#split usersname 
+first,last=name.split(" ")
+print(f"hello,{first}")
 #say hello to users
+print(name1, string)
 print("hello, World" +name)
 print("hello ,",end="")
 print(name)
 print("hello ,",name,sep="@@")
+print("hello,\"friend\"")
+print(f"name:{name}")
