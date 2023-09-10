7154aaf931f06ffae5b9832b46e686088bb27554
najah <najahaboobacker79@gmail.com>
2023-09-10 19:59:03 +0530

    12

---
+++ calculator.py
@@ -0,0 +1,11 @@
+x=int(input("enter the number "))
+#int() is converting string  into int
+y=int(input("enter the number "))
+print(x+y)
+a=float(input("enter the float"))
+b=float(input("enter the float"))
+#round the value to higher
+z=round(a+b)
+c=round(a/b,2)
+print("float value",z,c)
+print(f"{z:,}")
