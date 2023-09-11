aacc81dea884a8727c3e35788f967cb57c71a717
najah <najahaboobacker79@gmail.com>
2023-09-11 20:49:26 +0530

    43

--- func2.py
+++ func2.py
@@ -3,3 +3,9 @@ def hello(to):
 
 name=input("enter the name")
 hello(name)    
+
+#giving constant value to hello fucntion
+def hello1(to="hi"):
+    print("hello",to)
+hello1()
+    
---
+++ sqUARE.py
@@ -0,0 +1,8 @@
+def main():
+    x=int(input("enter the the number"))
+    print("squared number",square(x))
+    
+def square(n):
+    return n*n
+
+main()    
