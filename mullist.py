90db45c1656327264602bb632f8bb27917bd5094
najah <najahaboobacker79@gmail.com>
2023-09-14 17:02:44 +0530

    0

---
+++ mullist.py
@@ -0,0 +1,9 @@
+students = [
+    {"name":"najah","house":"daruslam","place":"kannur"},
+    {"name":"shahid","house":"allor","place":"malapuram"}
+]
+
+
+
+for student in students:
+    print(student["name"])
