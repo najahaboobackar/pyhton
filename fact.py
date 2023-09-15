ba296c31cd2499faf50adeab54df2100ad558727
najah <najahaboobacker79@gmail.com>
2023-09-15 15:37:21 +0530

    23

---
+++ fact.py
@@ -0,0 +1,9 @@
+n = int(input("enter the number"))
+for i in range(2, n+1):  # Start from 2
+    if n % i == 0:
+        count = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                count = count + 1
+        if count == 2:
+            print(j)
