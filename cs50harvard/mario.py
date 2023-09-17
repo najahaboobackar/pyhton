e2399c6ff06845a2c8326edd4e82cd9c24adc83a
najah <najahaboobacker79@gmail.com>
2023-09-17 23:18:36 +0530

    23

---
+++ cs50harvard/mario.py
@@ -0,0 +1,11 @@
+def main():
+   print_column(4)
+   
+    
+def print_column(h):
+    for _ in range(h):
+        print("#")    
+        
+        
+        
+main()        
---
+++ week2/alpha.py
@@ -0,0 +1,8 @@
+n = input("The character is ")
+
+if 'A' <= n <= 'Z':
+    print(1)
+elif 'a' <= n <= 'z':
+    print(0)
+else:
+    print(-1)
